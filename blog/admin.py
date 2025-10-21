596ad75ac8847fcdde61f66d493a35e93ebec5cc
ndikuma <ii61212456@gmail.com>
2025-10-21 18:14:53 +0200

    add les action

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Posts, Category, Comment
+from .models import Post, Category, Comment
 
 # Register your models here.
-admin.site.register(Posts)
+admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Comment)
